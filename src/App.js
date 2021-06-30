43e5e6fcc0c3cbe67a4883810111aacb1d99352a
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-06-30 17:08:09 +0100

    rough concept

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import {
   csv,
   scaleTime,
   scaleBand,
+  scaleLinear,
   max,
   extent,
   timeFormat,
-  tickFormat,
+  format,
 } from "d3";
+import randomColor from "randomcolor"
+
+import { csvUrl } from "./csvUrl"
 
 import "./style.css";
 
-const csvUrl =
-  "https://gist.githubusercontent.com/Lewisjohnward/7a8eb5a17366a2cc83e1c414e06c492b/raw/7fd767ebf67cf63b64ef92496978bd816820e48d/Lessons.csv";
 
-const width = 960;
-const height = 500;
+const width = window.innerWidth ;
+const height = window.innerHeight / 2;
 
 const margin = {
   top: 20,
-  right: 30,
+  right: 40,
   bottom: 65,
-  left: 90,
+  left: 40,
 };
 
 export const App = () => {
@@ -29,8 +31,9 @@ export const App = () => {
 
   useEffect(() => {
     const row = (d) => {
-      d.length = +d.length;
-      d.date = new Date(+d.date);
+      d.length = +d['length'];
+      d.date = new Date(+d['date'])
+      d.id = +d.id
       return d;
     };
     csv(csvUrl, row).then(setData);
@@ -39,10 +42,16 @@ export const App = () => {
   if (!data) {
     return <pre>Loading...</pre>;
   }
-  console.log(data);
+
+  data.sort(function(a, b) {
+    var c = new Date(a.date);
+    var d = new Date(b.date);
+    return c-d;
+  });
+
   console.log(data[0]);
 
-  const xTicksFormat = timeFormat("%Y");
+  const tickFormat = timeFormat("%Y");
 
   const innerHeight = height - margin.top - margin.bottom;
   const innerWidth = width - margin.left - margin.right;
@@ -50,65 +59,128 @@ export const App = () => {
   const xValue = (d) => d.date;
   const xScale = scaleTime()
     .domain(extent(data, xValue))
-    .range([0, innerWidth]);
+    .range([0, innerWidth])
+    .nice()
 
 
 
   const yValue = d => d.length
-  const yScale = scaleBand()
-    .domain(data.map(yValue))
+  const yScale = scaleLinear()
+    .domain([0, max(data, yValue) * 2])
     .range([0, innerHeight])
+    .nice()
 
   const tickOffset = 0;
 
-  //console.log(xScale.ticks());
-  console.log(yScale.bandwidth())
-  console.log(xScale(data[9].date))
+  console.log(yScale.ticks());
+  //console.log(yScale.bandwidth())
+  //console.log(xScale(data[0].date))
   return (
     <>
       <svg width={width} height={height}>
-        <g>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+
+
+          {/*X axis*/}
+          {xScale.ticks().map(tickValue => (
+            <g>
+              <line stroke="red" stroke-dasharray="5,5" transform={`translate(${xScale(tickValue)}, 0)`} y2={innerHeight}/>
+            <text
+              style={{textAnchor: "middle"}}
+              x={xScale(tickValue)}
+              y={innerHeight + 15}
+            >{tickFormat(tickValue)}</text>
+            </g>
+          ))}
 
 
 
-            {/*X axis*/}
-          {xScale.ticks().map((tickValue) => (
-            <g
-              transform={`translate(${xScale(tickValue)}, 0)`}
-              className="tick"
-            >
-              <line y2={innerHeight} />
+          {/*Y axis*/}
+          {yScale.ticks().map(tickValue => (
+            <g transform={`translate(0, ${innerHeight - yScale(tickValue)})`}> 
               <text
-                style={{ textAnchor: "middle" }}
-                dy=".71em"
-                y={innerHeight + 3}
+                style={{ textAnchor: 'end' }}
+                dy=".32em"
+                x={-2}
               >
-                {xTicksFormat(tickValue)}
+                {format(".2s")(tickValue)}
               </text>
             </g>
           ))}
 
+
+          
           {/*marks*/}
-          {data.map(d => (
-              <rect
-                x={xScale(d.date)}
-                y={yScale(d.length)}
-                width={yScale.bandwidth()}
-                height={xScale(d.length)} />
-            ))
-            }
+          {drawMarks(data, xScale, innerHeight, yScale)}
 
-
-
-
-
-
-        </g>
-      </svg>
-      <svg width={width} height={height}>
-      <rect x="50" y="20" width="150" height="150"/>
-
+          </g>
       </svg>
     </>
   );
 };
+
+const drawMarks = (data, xScale, innerHeight, yScale) => {
+  let previousDate = new Date()
+  let yCoords = innerHeight
+  let quantity = 0
+
+  const marks = data.map(d => {
+    quantity ++
+    const currentDate = d.date.getTime()
+
+    const rectangleY = innerHeight - yScale(d.length)
+
+    if (currentDate == previousDate){
+      console.log("Yes they match")
+      yCoords -= yScale(d.length)
+
+      return (
+        <g>
+          <text
+            style={{textAnchor: "middle"}}
+            x={xScale(d.date)}
+            y={-5}
+          >
+            {quantity % 100 === 0 ? quantity : null}
+          </text>
+          
+          <rect 
+            key={d.id} 
+            hello={d.id} 
+            x={xScale(d.date)} 
+            y={yCoords} 
+            width={1} 
+            height={yScale(d.length)}
+            fill={"black"}
+            />
+        </g>
+      )
+    }
+
+    previousDate = currentDate
+    yCoords = rectangleY
+
+      return (
+      <g>
+        <text
+          style={{textAnchor: "middle"}}
+          x={xScale(d.date)}
+          y={-5}
+        >
+          {quantity % 100 === 0 ? quantity : null}
+        </text>
+        <rect 
+          key={d.id} 
+          x={xScale(d.date)} 
+          y={rectangleY} 
+          width={1} 
+          height={yScale(d.length)}
+          fill={"black"}
+          />
+      </g>
+    )
+  
+  })
+
+    return marks
+}
---
+++ src/csvUrl.js
@@ -0,0 +1,4 @@
+export const csvUrl =
+  "https://gist.githubusercontent.com/Lewisjohnward/7a8eb5a17366a2cc83e1c414e06c492b/raw/7fd767ebf67cf63b64ef92496978bd816820e48d/Lessons.csv";
+
+//export const csvUrl = "https://gist.githubusercontent.com/Lewisjohnward/b5c8419ff2047c2671a70ae77dc03c6c/raw/3eb17e26634d007f087de7b37a4514ad1bddd5b4/WPP2019.csv"
